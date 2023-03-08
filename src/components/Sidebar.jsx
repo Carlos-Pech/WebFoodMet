247881bb8614b9c5b76b50c77613b820b3c69640
Carlos-Pech <carlospech247@gmail.com>
2023-03-07 21:30:06 -0600

    Log out

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { RiFolderChart2Line,RiLogoutBoxRLine,RiHome3Line,RiRestaurantLine,RiArchiveLine } from "react-icons/ri";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  RiFolderChart2Line,
+  RiLogoutBoxRLine,
+  RiHome3Line,
+  RiRestaurantLine,
+  RiArchiveLine,
+} from "react-icons/ri";
 
 const Sidebar = () => {
+  function handleLogout() {
+    localStorage.removeItem("token");
+    // redirigir a la página de inicio de sesión o hacer cualquier otra acción necesaria después de cerrar sesión
+  }
+
   return (
-    <div className='xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100 p-12 flex flex-col justify-between'>
+    <div className="xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full -left-full top-0 bg-secondary-100 p-12 flex flex-col justify-between">
       <div>
-        <h1 className='text-center text-2xl font-bold text-white mb-10'>
-          FoodMet<span className='text-primary texxt-4xl'>.</span>
-          
+        <h1 className="text-center text-2xl font-bold text-white mb-10">
+          FoodMet<span className="text-primary texxt-4xl">.</span>
         </h1>
-        <nav className=''>
-          <Link to="/home" className=' flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors'
+        <nav className="">
+          <Link
+            to="/home"
+            className=" flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors"
           >
-            <RiHome3Line  className='text-primary'/>Home
+            <RiHome3Line className="text-primary" />
+            Home
           </Link>
 
-          <Link to="platillos" className=' flex items-center gap-4 py-4 px-4 rounded-lg  hover:bg-purple-700 transition-colors'
+          <Link
+            to="platillos"
+            className=" flex items-center gap-4 py-4 px-4 rounded-lg  hover:bg-purple-700 transition-colors"
           >
-            <RiRestaurantLine  className='text-primary'/>Platillos
+            <RiRestaurantLine className="text-primary" />
+            Platillos
           </Link>
-          <Link to="ingredientes" className=' flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors'
+          <Link
+            to="ingredientes"
+            className=" flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors"
           >
-            <RiArchiveLine  className='text-primary'/>Ingredientes
+            <RiArchiveLine className="text-primary" />
+            Ingredientes
           </Link>
           {/* <Link to="/" className=' flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors'
           >
@@ -31,13 +50,20 @@ const Sidebar = () => {
         </nav>
       </div>
       <nav>
-        <Link to="/login" className=' flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors'
-          >
-          <RiLogoutBoxRLine  className='text-primary'/>Cerrar sesion
-        </Link>
+        <button onClick={handleLogout}>
+          <button onClick={handleLogout}>
+            <Link
+              to="/"
+              className=" flex items-center gap-4 py-4 px-4 rounded-lg hover:bg-purple-700 transition-colors"
+            >
+              <RiLogoutBoxRLine className="text-primary" />
+              Cerrar sesion
+            </Link>
+          </button>
+        </button>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
